ea6c1e938f956b78d5e71a1c034203c2f946d66b
jakobSjoberg <jaksjob@chalmers.se>
2025-04-28 14:59:56 +0200

    First implementation of problem 3

--- Assignment3/assignment3_problem1_skeleton.py
+++ Assignment3/assignment3_problem1_skeleton.py
@@ -3,7 +3,20 @@
 from mrjob.job import MRJob
 
 class MRMineral(MRJob):
-    pass # write your implementation here
+    def mapper(self, _, line):
+        line = line.split(",")
+        if line[0] == "Constellation": #This is the header
+            return
+        if line[1] == "Prime":
+            yield(line[0], int(line[6])) #(StarConstellation, RU)
+        else:
+            yield(line[1] + " " + line[0], int(line[6])) #(StarConstellation, RU)
+
+    def combiner(self, star, RUs):
+        yield(star, sum(RUs))
+
+    def reducer(self, star, RUs):
+        yield(star, sum(RUs))
 
 if __name__ == '__main__':
     MRMineral().run()
