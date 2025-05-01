1ca294339231974e6c9c080785718536b5cb998d
jakobSjoberg <jaksjob@chalmers.se>
2025-05-01 14:22:19 +0200

    problem3 initial

--- Assignment3/assignment3_problem2.py
+++ Assignment3/assignment3_problem2.py
@@ -6,14 +6,15 @@ from mrjob.job import MRStep
 class MRMineral(MRJob):
     def configure_args(self):
         super(MRMineral, self).configure_args()
-        self.add_passthru_arg("-k", "--k-most-valuable", type=int, default=0, help="List the k most valuable starsystems")
+        self.add_passthru_arg("-k", "--k-most-valuable", type=int, default=0, 
+            help="List the k most valuable starsystems, outputs all star systems in arbitrary order if 0 or empty.")
 
     def mapper(self, _, line):
         line = line.split(",")
         if line[0] == "Constellation": #This is the header, ignore
             pass
         else:
-            yield(line[0] if line[1] == "Prime" else line[1] + " " + line[0], int(line[5])) #(StarConstellation, RU)
+            yield(line[0] if line[1] == "Prime" else line[1] + " " + line[0], int(line[5])) 
 
     def combiner(self, star, RUs):
         yield(star, sum(RUs))
@@ -21,13 +22,16 @@ class MRMineral(MRJob):
     def reducer(self, star, RUs):
         yield(None, (star, sum(RUs)))
 
+    # Yields the k star systems with the largest RU values in descending order
+    # or all star systems in arbitrary order if k == 0
     def reducer2(self, _, stars):
         k = self.options.k_most_valuable
         if k > 0:
             top_k = []
             for star, total_RU in stars:
                 if len(top_k) < k:
-                    top_k.append((total_RU, star))
+                    # Append in this order to make RUs the value compared on min() function
+                    top_k.append((total_RU, star)) 
                 else:
                     min_RU = min(top_k)
                     if (total_RU, star) > min(top_k):
@@ -36,14 +40,15 @@ class MRMineral(MRJob):
                         
             top_k.sort(reverse=True)
             for i in range(k):
-                yield(top_k[i])
+                yield(top_k[i][1], top_k[i][0]) # Yield in order starsystem, RU
                     
         else:
             for (star, total_RU) in stars:
                 yield(star, total_RU)
 
     def steps(self):
-        return [MRStep(mapper=self.mapper, combiner=self.combiner, reducer=self.reducer), MRStep(reducer=self.reducer2)]
+        return [MRStep(mapper=self.mapper, combiner=self.combiner, reducer=self.reducer), 
+                MRStep(reducer=self.reducer2)]
     
 if __name__ == '__main__':
     MRMineral().run()
